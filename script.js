fe3c2aaa515aa4155daa3dcd954d89b6e013e486
Ana Luíza <analluizafr@gmail.com>
2023-05-17 00:02:41 -0300

    Salvos 16/05

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-//Atividade 01. 
-//Questão 1
-
+//Questão 5
 function calcule(numero){
-return parseFloat(prompt(numero))
+    return parseFloat(prompt(numero))
 }
 
-var num1 = calcule("Digite um número:");
-var num2 = calcule("Digite um número:");
-var num3 = calcule("Digite um número:");
- 
-var média =(num1+num2+num3)/3
- window.prompt("A média aritimética é:" + média);
 
+var num = calcule("Digite seu nome:");
+var num2 = calcule("Digite sua nota de Matemática:");
+var num3 = calcule("Digite sua nota de Portugês:");
+var num4 = calcule("Digite sua nota de Biologia:");
+
+ if(média<8)window.prompt("Está reprovado!");
+ else(média>8);window.prompt("Está aprovado!")
+
+var média =(num2+num3+num4)/3
+ window.prompt("A média é:" + média +num);
+ 
 
